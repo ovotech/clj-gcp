afa57d498823d1c8b877d3069a9ee1360eb7b020
oli <oli.desborough@ovoenergy.com>
2020-08-27 12:36:34 +0100

    Upgrade clj-gcp

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 
                  [cheshire "5.8.0"]
                  [com.google.cloud/google-cloud-pubsub "1.40.0"]
-                 [com.google.cloud/google-cloud-storage "1.40.0"]
+                 [com.google.cloud/google-cloud-storage "1.111.2"]
                  [digest "1.4.8"]
                  [iapetos "0.1.8"]
                  [integrant "0.6.3"]
