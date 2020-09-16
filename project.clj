621143f3893ab50a7143437bbcfd61a129585c10
Markus <m.walther97@gmail.com>
2020-09-16 16:16:43 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/clj-gcp "0.6.5"
+(defproject ovotech/clj-gcp "0.6.6"
   :description "Clojure utilities for the Google Cloud Platform"
 
   :url "https://github.com/ovotech/clj-gcp"
