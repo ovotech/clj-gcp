d407396f3099b5559c673f28c964b838c3aa120f
oli <oli.desborough@ovoenergy.com>
2020-11-03 14:14:47 +0000

    Bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/clj-gcp "0.6.7"
+(defproject ovotech/clj-gcp "0.6.8"
   :description "Clojure utilities for the Google Cloud Platform"
 
   :url "https://github.com/ovotech/clj-gcp"
