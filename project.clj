ced319e2b5eae535d02b729c31d454afbe169db9
GavinEsbergerOVO <39487663+GavinEsbergerOVO@users.noreply.github.com>
2020-11-16 17:56:17 +0000

    Add cause to ex-info when blob missing (#29)
    * Add cause to ex-info when blob missing
    
    * updating patch version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/clj-gcp "0.6.11"
+(defproject ovotech/clj-gcp "0.6.12"
   :description "Clojure utilities for the Google Cloud Platform"
 
   :url "https://github.com/ovotech/clj-gcp"
