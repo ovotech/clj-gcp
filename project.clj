1deefe59691771dccc5d0fb231d46aa6bfaf13cc
oli-d-ovo <oli-d-ovo@users.noreply.github.com>
2021-03-14 00:34:59 +0000

    deps: upgrade to newer versions

--- project.clj
+++ project.clj
@@ -10,16 +10,16 @@
 
                  [cheshire "5.10.0"]
                  [clj-commons/fs "1.6.307"]
-                 [com.google.cloud/google-cloud-pubsub "1.110.3"]
+                 [com.google.cloud/google-cloud-pubsub "1.111.4"]
                  [com.google.oauth-client/google-oauth-client "1.31.4"]
-                 [com.google.cloud/google-cloud-storage "1.113.8"
+                 [com.google.cloud/google-cloud-storage "1.113.14"
                   :exclusions [com.google.oauth-client/google-oauth-client]]
                  [digest "1.4.10"]
                  [iapetos "0.1.8"]
                  [integrant "0.8.0"]
                  [medley "1.3.0"]
-                 [metosin/ring-http-response "0.9.1"]
-                 [org.clojure/clojure "1.10.1"]
+                 [metosin/ring-http-response "0.9.2"]
+                 [org.clojure/clojure "1.10.3"]
                  [org.clojure/spec.alpha "0.2.194"]
                  [org.clojure/tools.logging "1.1.0"]]
 
@@ -29,8 +29,8 @@
   :test-selectors {:default     (complement :integration)
                    :integration :integration}
 
-  :profiles {:dev {:dependencies [[expound "0.8.7"]]
-                   :plugins [[lein-ancient "0.6.15"]]}
+  :profiles {:dev {:dependencies [[expound "0.8.9"]]
+                   :plugins [[lein-ancient "0.7.0"]]}
              :ci  {:deploy-repositories
                    [["clojars" {:url           "https://clojars.org/repo"
                                 :username      :env ;; LEIN_USERNAME
